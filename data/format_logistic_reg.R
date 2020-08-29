298dd8a500944d6c34fec63e6ca527ecdd6909ff
Richard Chandler <richard.chandlers@gmail.com>
2020-08-29 17:10:26 -0400

    prepare CAWA GLM data

--- data/format_logistic_reg.R
+++ data/format_logistic_reg.R
@@ -81,80 +81,72 @@ str(det.arr)
 
 
 ## TODO: Write a function to do this for any species
-dets.cawa.14 <- det.arr[,,"2014","CAWA"]
-dets.cawa.15 <- det.arr[,,"2015","CAWA"]
-dets.cawa.16 <- det.arr[,,"2016","CAWA"]
-dets.cawa.17 <- det.arr[,,"2017","CAWA"]
-dets.cawa.18 <- det.arr[,,"2018","CAWA"]
-dets.cawa.19 <- det.arr[,,"2019","CAWA"]
-dets.cawa.20 <- det.arr[,,"2020","CAWA"]
-
-cawa.14 <- data.frame(y=ifelse(rowSums(dets.cawa.14)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.14),
-                      sites[rownames(dets.cawa.14),],
-                      Year="2014")
-str(cawa.14)
-cawa.15 <- data.frame(y=ifelse(rowSums(dets.cawa.15)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.15),
-                      sites[rownames(dets.cawa.15),],
-                      Year="2015")
-cawa.16 <- data.frame(y=ifelse(rowSums(dets.cawa.16)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.16),
-                      sites[rownames(dets.cawa.16),],
-                      Year="2016")
-cawa.17 <- data.frame(y=ifelse(rowSums(dets.cawa.17)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.17),
-                      sites[rownames(dets.cawa.17),],
-                      Year="2017")
-cawa.18 <- data.frame(y=ifelse(rowSums(dets.cawa.18)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.18),
-                      sites[rownames(dets.cawa.18),],
-                      Year="2018")
-cawa.19 <- data.frame(y=ifelse(rowSums(dets.cawa.19)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.19),
-                      sites[rownames(dets.cawa.19),],
-                      Year="2019")
-cawa.20 <- data.frame(y=ifelse(rowSums(dets.cawa.20)>0, 1L, 0L),
-                      Site=rownames(dets.cawa.20),
-                      sites[rownames(dets.cawa.20),],
-                      Year="2020")
-str(cawa.20)
-
-cawa <- rbind(cawa.14, cawa.15, cawa.16, cawa.17, cawa.18, cawa.19, cawa.20)
+formatLogisticRegData <- function(detArray, sites, species) {
+    dim.dets <- dim(detArray)
+    nYears <- dim.dets[3]
+    yrs <- dimnames(detArray)[[3]]
+    dets <- detArray[,,1,species]
+    counts.yr1 <- rowSums(dets)
+    dat <- data.frame(abundance=counts.yr1,
+                      presence=ifelse(counts.yr1>0, 1L, 0L),
+                      sites[rownames(dets),],
+                      year=yrs[1])
+    for(t in 2:nYears) {
+        dets.t <- detArray[,,t,species]
+        counts.yr.t <- rowSums(dets.t)
+        dat.t <- data.frame(abundance=counts.yr.t,
+                            presence=ifelse(counts.yr.t>0, 1L, 0L),
+                            sites[rownames(dets.t),],
+                            year=yrs[t])
+        dat <- rbind(dat, dat.t)
+    }
+    colnames(dat) <- c("abundance", "presence", "utmN", "utmW", "elevation", "year")
+    return(dat)
+}
+        
 
 
-str(cawa)
+cawaData <- formatLogisticRegData(det.arr, sites, "CAWA")
+howaData <- formatLogisticRegData(det.arr, sites, "HOWA")
+veerData <- formatLogisticRegData(det.arr, sites, "VEER")
+wothData <- formatLogisticRegData(det.arr, sites, "WOTH")
+
+str(cawaData)
+
+
+write.csv(cawaData, file="cawa_data_glm.csv")
 
 
 
+fm1 <- glm(presence ~ elevation+I(elevation^2), binomial, cawaData)
+fm2 <- glm(presence ~ year, binomial, cawaData)
+fm3 <- glm(presence ~ elevation+I(elevation^2)+year, binomial, cawaData)
+fm4 <- glm(presence ~ (elevation+I(elevation^2))*year, binomial, cawaData)
+fm5 <- glm(presence ~ elevation*year, binomial, cawaData)
+fm6 <- glm(presence ~ elevation+year, binomial, cawaData)
 
-fm1 <- glm(y ~ Elevation+I(Elevation^2), binomial, cawa)
-fm2 <- glm(y ~ Year, binomial, cawa)
-fm3 <- glm(y ~ Elevation+I(Elevation^2)+Year, binomial, cawa)
-fm4 <- glm(y ~ (Elevation+I(Elevation^2))*Year, binomial, cawa)
-
-AIC(fm1, fm2, fm3, fm4)
+AIC(fm1, fm2, fm3, fm4, fm5, fm6)
 
 summary(fm3)
 
-elev.min <- min(cawa$Elevation)
-elev.max <- max(cawa$Elevation)
+elev.min <- min(cawaData$elevation)
+elev.max <- max(cawaData$elevation)
 
 yrs <- 2014:2020
 
-pred.data <- data.frame(Year=factor(rep(as.character(yrs), each=50)),
-                        Elevation=rep(seq(elev.min, elev.max, length=50),
+pred.data <- data.frame(year=factor(rep(as.character(yrs), each=50)),
+                        elevation=rep(seq(elev.min, elev.max, length=50),
                                       times=length(yrs)))
 
 pred3 <- predict(fm3, newdata=pred.data, type="response", se.fit=TRUE)
 
 pred3dat <- data.frame(pred=pred3$fit, pred.data)
 
-plot(pred ~ Elevation, pred3dat, subset=Year=="2014",
+plot(pred ~ elevation, pred3dat, subset=year=="2014",
      type="l", ylim=c(0, 1))
-lines(pred ~ Elevation, pred3dat, subset=Year=="2015", col=2)
-lines(pred ~ Elevation, pred3dat, subset=Year=="2016", col=3)
-lines(pred ~ Elevation, pred3dat, subset=Year=="2017", col=4)
-lines(pred ~ Elevation, pred3dat, subset=Year=="2018", col=5)
-lines(pred ~ Elevation, pred3dat, subset=Year=="2019", col=6)
-lines(pred ~ Elevation, pred3dat, subset=Year=="2020", col=7)
+lines(pred ~ elevation, pred3dat, subset=year=="2015", col=2)
+lines(pred ~ elevation, pred3dat, subset=year=="2016", col=3)
+lines(pred ~ elevation, pred3dat, subset=year=="2017", col=4)
+lines(pred ~ elevation, pred3dat, subset=year=="2018", col=5)
+lines(pred ~ elevation, pred3dat, subset=year=="2019", col=6)
+lines(pred ~ elevation, pred3dat, subset=year=="2020", col=7)
