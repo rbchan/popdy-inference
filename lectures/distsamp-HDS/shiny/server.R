7c0b8e5acde3fb9092280d6d475c8ea7ab55616d
Richard Chandler <richard.chandlers@gmail.com>
2021-10-11 21:04:03 -0400

    distsamp lecture with app

--- lectures/distsamp-HDS/shiny/server.R
+++ lectures/distsamp-HDS/shiny/server.R
@@ -3,27 +3,48 @@ function(input, output, session) {
     output$gx <- renderPlot({
         sig <- input$sigma
         plot(function(x) exp(-x^2 / (2*sig^2)), from=0, to=input$distanceMax,
-             ylim=c(0, 1), xlab="Distance (x)", ylab="g(x)", main="Detection probability")
+             ylim=c(0, 1), col="blue", lwd=2,
+             xlab="Distance (x)", ylab="g(x)", main="Detection probability")
     })
 
     output$px <- renderPlot({
-        plot(function(x) 1/rep(input$distanceMax, length(x)),
-             from=0, to=input$distanceMax, ylim=c(0, 2/input$distanceMax),
-             xlab="Distance (x)", ylab="p(x)", main="p(animal occurs at distance x)")
+        if(input$transect=="Line transect") {
+            plot(function(x) 1/rep(input$distanceMax, length(x)),
+                 from=0, to=input$distanceMax, ylim=c(0, 2/input$distanceMax),
+                 col="orange", lwd=2,
+                 xlab="Distance (x)", ylab="p(x)", main="p(animal occurs at distance x)")
+        } else {
+            plot(function(x) 2*x/input$distanceMax^2,
+                 from=0, to=input$distanceMax, ylim=c(0, 2/input$distanceMax),
+                 col="orange", lwd=2,
+                 xlab="Distance (x)", ylab="p(x)", main="p(animal occurs at distance x)")
+        }
     })
 
     output$gxpx <- renderPlot({
         sig <- input$sigma
-        plot(function(x) exp(-x^2 / (2*sig^2))/input$distanceMax,
-             from=0, to=input$distanceMax, ylim=c(0, 1/input$distanceMax),
-             xlab="Distance (x)", ylab="g(x)p(x)",
-             main="p(animal occurs and is detected at distance x)")
+        if(input$transect=="Line transect") {
+            plot(function(x) exp(-x^2 / (2*sig^2))/input$distanceMax,
+                 from=0, to=input$distanceMax, ylim=c(0, 1/input$distanceMax),
+                 col="seagreen", lwd=2,
+                 xlab="Distance (x)", ylab="g(x)p(x)",
+                 main="p(animal occurs and is detected at distance x)")
+        } else {
+            plot(function(x) exp(-x^2 / (2*sig^2))*(2*x/input$distanceMax^2),
+                 from=0, to=input$distanceMax, ylim=c(0, 2/input$distanceMax),
+                 col="seagreen", lwd=2,
+                 xlab="Distance (x)", ylab="g(x)p(x)",
+                 main="p(animal occurs and is detected at distance x)")
+        }
     })
 
     output$pbar <- renderTable({
         sig <- input$sigma
-        g <- function(x) exp(-x^2/(2*sig^2))    ## g(x)
-        pdf <- function(x) 1/input$distanceMax  ## p(x)
+        g <- function(x) exp(-x^2/(2*sig^2))            ## g(x)
+        if(input$transect=="Line transect") 
+            pdf <- function(x) 1/input$distanceMax      ## p(x) 
+        else 
+            pdf <- function(x) 2*x/input$distanceMax^2  ## p(x)
         gp <- function(x) g(x)*pdf(x)
         pbar <- integrate(gp, lower=0, upper=input$distanceMax)$value
         pbarOut <- rbind("pbar" = pbar)
--- lectures/distsamp-HDS/shiny/ui.R
+++ lectures/distsamp-HDS/shiny/ui.R
@@ -1,6 +1,7 @@
 pageWithSidebar(
-    titlePanel('Distance sampling'),
+    titlePanel('Distance sampling (half-normal detection function)'),
     sidebarPanel(
+        selectInput('transect', 'Survey type', c("Line transect", "Point transect")),
         numericInput('sigma', 'Scale parameter', 20, min=0, max=1000, step=5),
         numericInput('distanceMax', 'Maximum distance', 100, min=0, max=500, step=10),
     ),
