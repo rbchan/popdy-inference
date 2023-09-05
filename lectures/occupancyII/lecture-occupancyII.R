87b8e1eb7488d9ded4e4bd3180b7265d0632c0a9
rbchan <richard.chandlers@gmail.com>
2023-09-05 14:33:25 -0400

    occupancy

--- lectures/occupancyII/lecture-occupancyII.R
+++ lectures/occupancyII/lecture-occupancyII.R
@@ -6,7 +6,7 @@
 
 
 ## ----sim-cov1,size='scriptsize'-----------------------------------------------
-nSites <- 100; nVisits <- 4; set.seed(3439) ## Make it reproducible
+nSites <- 100; nVisits <- 4; set.seed(83) ## Make it reproducible
 x1 <- rnorm(nSites,0,0.5); x2 <- rnorm(nSites,100,10) ## Continuous covs
 w <- matrix(sample(c("Cold", "Hot"), size=nSites*nVisits, replace=T),
             nrow=nSites, ncol=nVisits)
@@ -14,9 +14,9 @@ wHot <- ifelse(w=="Hot", 1, 0)              ## Dummy variable
 
 
 ## ----nsim-cov2,size='scriptsize'----------------------------------------------
-beta0 <- 1; beta1 <- -1; beta2 <- 0
+beta0 <- 0; beta1 <- -1; beta2 <- 0
 psi <- plogis(beta0 + beta1*x1 + beta2*x2)
-alpha0 <- 0; alpha1 <- 1; alpha2 <- 0.5
+alpha0 <- -0.5; alpha1 <- 1; alpha2 <- 2
 p <- plogis(alpha0 + alpha1*x1 + alpha2*wHot)
 
 
@@ -45,7 +45,7 @@ naiveOccupancy
 library(unmarked)
 
 
-## ----un-umf,size='tiny'-------------------------------------------------------
+## ----un-umf,size='tiny',warning=FALSE-----------------------------------------
 umf <- unmarkedFrameOccu(y=y, siteCovs=data.frame(x1,x2), obsCovs=list(w=w))
 
 
@@ -53,9 +53,11 @@ umf <- unmarkedFrameOccu(y=y, siteCovs=data.frame(x1,x2), obsCovs=list(w=w))
 summary(umf)
 
 
-## ----umf-zcov,size='small'----------------------------------------------------
-siteCovs(umf)$x1s <- (x1-mean(x1))/sd(x1)
-siteCovs(umf)$x2s <- (x2-mean(x2))/sd(x2)
+## ----umf-zcov,size='footnotesize'---------------------------------------------
+mean.x1 <- mean(x1); mean.x2 <- mean(x2)
+sd.x1 <- sd(x1); sd.x2 <- sd(x2)
+siteCovs(umf)$x1s <- (x1-mean.x1)/sd.x1
+siteCovs(umf)$x2s <- (x2-mean.x2)/sd.x2
 
 
 ## ----un-fit,size='tiny'-------------------------------------------------------
@@ -64,21 +66,23 @@ fm
 
 
 ## ----un-compare,size='tiny'---------------------------------------------------
-c(beta0=beta0, beta1=beta1, beta2=beta2)
-c(alpha0=alpha0, alpha1=alpha1, alpha2=alpha2)
+rbind('Occupancy coefs (beta)'=c(beta0, beta1, beta2),
+      'Detection coefs (alpha)'=c(alpha0, alpha1, alpha2))
 
 
-## ----preddat,size='small'-----------------------------------------------------
+## ----preddat,size='footnotesize'----------------------------------------------
 pred.data <- data.frame(x1s=seq(from=-3, to=3, length=50),
-                        x2s=0, w='Hot') 
+                        x2s=0, w='Hot')
+pred.data$x1 <- pred.data$x1s*sd.x1+mean.x1 ## Back-transform x1s
+pred.data$x2 <- pred.data$x2s*sd.x2+mean.x2 ## Back-transform x2s
 
 
-## ----predpsi,size='small'-----------------------------------------------------
+## ----predpsi,size='footnotesize'----------------------------------------------
 psi.pred <- predict(fm, newdata=pred.data,
                     type='state', append=TRUE)
 
 
-## ----predp,size='small'-------------------------------------------------------
+## ----predp,size='footnotesize'------------------------------------------------
 p.pred <- predict(fm, newdata=pred.data,
                   type='det', append=TRUE)
 
@@ -91,37 +95,29 @@ print(head(psi.pred), digits=2)
 print(head(p.pred), digits=2)
 
 
-## ----pred-psi1,fig.width=7,fig.height=5.5,size='tiny',out.width='80%',fig.align='center'----
+## ----pred-psi1,fig.width=7,fig.height=5,size='tiny',out.width='80%',fig.align='center',echo=-1----
+par(mai=c(0.9,0.9,0.1,0.1))  
 plot(Predicted ~ x1s, psi.pred, type="l", ylab="Occurrence probability", col="blue",
      xlab="Standardized covariate (x1s)", ylim=0:1) 
 lines(lower ~ x1s, psi.pred, col="grey"); lines(upper ~ x1s, psi.pred, col="grey")
 
 
-## ----pred-psi1s,fig.width=7,fig.height=5.5,size='tiny',out.width='80%',fig.align='center'----
-plot(Predicted ~ x1s, psi.pred, type="l", ylab="Occurrence probability", col="blue",
-     xlab="Original scale covariate (x1)", ylim=0:1, xaxt="n") ## Suppress x-axis
-x1s.ticks <- -3:3  ## These are where tick marks for x1s would be
-axis(side=1, at=x1s.ticks, labels=round(x1s.ticks*sd(x1)+mean(x1),1)) ## Backtransform x1s
-lines(lower ~ x1s, psi.pred, col="grey"); lines(upper ~ x1s, psi.pred, col="grey")
+## ----pred-psi1s,fig.width=7,fig.height=5,size='tiny',out.width='80%',fig.align='center',echo=-1----
+par(mai=c(0.9,0.9,0.1,0.1))  
+plot(Predicted ~ x1, psi.pred, type="l", ylab="Occurrence probability", col="blue",
+     xlab="Original scale covariate (x1)", ylim=0:1)
+lines(lower ~ x1, psi.pred, col="grey"); lines(upper ~ x1, psi.pred, col="grey")
 
 
-## ----pred-p1,fig.width=7,fig.height=5.5,size='tiny',out.width='80%',fig.align='center'----
-plot(Predicted ~ x1s, p.pred, type="l", ylab="Detection probability", col="purple",
-     xlab="Original scale covariate (x1)", ylim=0:1, xaxt="n")
-axis(side=1, at=x1s.ticks, labels=round(x1s.ticks*sd(x1)+mean(x1),1)) ## Backtransform x1s
-lines(lower ~ x1s, p.pred, col="grey")
-lines(upper ~ x1s, p.pred, col="grey")
+## ----pred-plot2,fig.width=7,fig.height=5,size='tiny',out.width='70%',fig.align='center',echo=-1----
+par(mai=c(0.9,0.9,0.1,0.1))  
+plot(Predicted ~ x1, psi.pred, type="l", ylab="Probability", col="blue", ylim=0:1,
+     xlab="x1", lwd=2)
+lines(Predicted ~ x1, p.pred, col="orange", lwd=2)
+legend(-3, 0.3, c("Occupancy (psi)", "Detection (p)"), lty=c(1, 1), col=c("blue", "orange"), lwd=2)
 
 
-## ----pred-plot2,fig.width=7,fig.height=5.5,size='tiny',out.width='70%',fig.align='center'----
-plot(Predicted ~ x1s, psi.pred, type="l", ylab="Probability", col="blue", ylim=0:1,
-     xlab="x1", xaxt="n")
-axis(side=1, at=x1s.ticks, labels=round(x1s.ticks*sd(x1)+mean(x1),1)) ## Backtransform x1s
-lines(Predicted ~ x1s, p.pred, col="purple")
-legend(-3, 0.75, c("psi", "p"), lty=c(1, 1), col=c("blue", "purple"))
-
-
-## ----bugs,size='scriptsize',echo=FALSE----------------------------------------
+## ----bugs,size='scriptsize',echo=FALSE,background='lightblue',comment=''------
 writeLines(readLines("occupancy-model-covs.jag"))
 
 
@@ -142,7 +138,7 @@ jags.pars <- c("beta0", "beta1", "beta2",
                "alpha0", "alpha1", "alpha2", "sitesOccupied")
 
 
-## ----bugs-mcmc,size='scriptsize',message=FALSE,cache=TRUE---------------------
+## ----bugs-mcmc,size='scriptsize',message=FALSE,cache=FALSE--------------------
 library(jagsUI)
 jags.post.samples <- jags.basic(data=jags.data, inits=jags.inits,
                                 parameters.to.save=jags.pars,
@@ -181,23 +177,23 @@ for(i in 1:n.iter) {
 
 
 ## ----psi-pred1,size='scriptsize',fig.align='center',out.width='80%',fig.height=5,dev='png',dpi=200----
-plot(pred.data$x1s, psi.post.pred[1,], type="l", xlab="x1s",
+plot(pred.data$x1, psi.post.pred[1,], type="l", xlab="x1s",
      ylab="Occurrence probability", ylim=c(0, 1), col=gray(0.8))
 
 
-## ----psi-pred-post,size='scriptsize',fig.align='center',out.width='80%',fig.height=5,echo=-1,dev='png',cache=TRUE,dpi=200----
-plot(pred.data$x1s, psi.post.pred[1,], type="l", xlab="x1s",
+## ----psi-pred-post,size='scriptsize',fig.align='center',out.width='80%',fig.height=5,echo=-1,dev='png',cache=FALSE,dpi=200----
+plot(pred.data$x1, psi.post.pred[1,], type="l", xlab="x1s",
      ylab="Occurrence probability", ylim=c(0, 1), col=gray(0.8))
 for(i in 1:n.iter) {
-    lines(pred.data$x1s, psi.post.pred[i,], col=gray(0.8))
+    lines(pred.data$x1, psi.post.pred[i,], col=gray(0.8))
 }
 
 
-## ----psi-pred-post-meanCI,size='tiny',fig.align='center',out.width='80%',fig.height=5,echo=-(1:2),dev='png',cache=TRUE,dpi=200----
-plot(pred.data$x1s, psi.post.pred[1,], type="l", xlab="x1s",
+## ----psi-pred-post-meanCI,size='tiny',fig.align='center',out.width='80%',fig.height=5,echo=-(1:2),dev='png',cache=FALSE,dpi=200----
+plot(pred.data$x1, psi.post.pred[1,], type="l", xlab="x1s",
      ylab="Occurrence probability", ylim=c(0, 1), col=gray(0.8))
 for(i in 1:n.iter) {
-    lines(pred.data$x1s, psi.post.pred[i,], col=gray(0.8))
+    lines(pred.data$x1, psi.post.pred[i,], col=gray(0.8))
 }
 pred.post.mean <- colMeans(psi.post.pred)
 pred.post.lower <- apply(psi.post.pred, 2, quantile, prob=0.025)
