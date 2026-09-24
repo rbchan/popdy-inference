e9d2e55b0f77e772c4dd27bdcf56a69e298c9e9b
rbchan <richard.chandlers@gmail.com>
2026-09-24 08:03:43 -0400

    shiny app

--- lectures/distsamp-HDS/shiny/deploy.R
+++ lectures/distsamp-HDS/shiny/deploy.R
@@ -12,5 +12,26 @@ if(1==2) {
 
     ## If getwd() is up one level
     runApp("shiny")
-    deployApp("shiny", appName="distance-sampling")
+    deployApp(appDir="shiny", appName="distance-sampling")
+}
+
+
+if(1==3) {
+
+## Move up one dir level
+rsconnect::migrateToConnectCloud(
+  appPath = "shiny",
+  contentId = "01a0d342-62e7-2e67-b9bc-3d69064dd322"
+)
+    
+## rsconnect::migrateToConnectCloud(
+##   appPath = "shiny",
+##   contentId = "abc123",
+##   # Pass only the argument(s) needed to single out the record;
+##   # often appName alone is enough.
+##   appName = "my-dashboard",
+##   account = "my-shinyapps-account",
+##   server = "shinyapps.io"
+## )    
+
 }
